09470a4c3b37f2cee9c3ddc6b0d5014aff186af9
Almaha <mahaw999@gmail.com>
2022-11-01 23:14:24 +0300

    adjust the code

--- main.js
+++ main.js
@@ -2,12 +2,13 @@ const advP = document.querySelector('.the-adv');
 const advNo = document.querySelector('.adv-no');
 const btn = document.querySelector('.btn');
 
-btn.addEventListener('click', ()=>{
-    fetch('https://api.adviceslip.com/advice')
-    .then((res)=> {return res.json()})
-    .then(data => {
-        advP.innerHTML = data.slip.advice;
-        advNo.innerHTML = `#${data.slip.id}`
+    advP.innerHTML = 'L O A D I N G...';
+    btn.addEventListener('click', ()=>{
+        fetch('https://api.adviceslip.com/advice')
+        .then((res)=> {return res.json()})
+        .then(data => {
+            advP.innerHTML = data.slip.advice;
+            advNo.innerHTML = `#${data.slip.id}`
+        })
+        .catch(e => console.log('ERROR', e))
     })
-    .catch(e => console.log('ERROR', e))
-})
